ba8c06dcc927122868e6f79510bafe1634690610
Serdar Eyüp ALTIN <43209941+serdaraltin@users.noreply.github.com>
2018-12-13 08:13:50 +0300

    Add files via upload

---
+++ Frekans Analizi/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Frekans_Analizi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tx_analizmetin = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstb_analizsonucu = new System.Windows.Forms.ListBox();
+            this.prgb_durum = new System.Windows.Forms.ProgressBar();
+            this.btn_analizet = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tx_analizmetin
+            // 
+            this.tx_analizmetin.Location = new System.Drawing.Point(82, 9);
+            this.tx_analizmetin.Multiline = true;
+            this.tx_analizmetin.Name = "tx_analizmetin";
+            this.tx_analizmetin.Size = new System.Drawing.Size(307, 238);
+            this.tx_analizmetin.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Analiz Metni";
+            // 
+            // lstb_analizsonucu
+            // 
+            this.lstb_analizsonucu.FormattingEnabled = true;
+            this.lstb_analizsonucu.Location = new System.Drawing.Point(395, 9);
+            this.lstb_analizsonucu.Name = "lstb_analizsonucu";
+            this.lstb_analizsonucu.Size = new System.Drawing.Size(96, 264);
+            this.lstb_analizsonucu.TabIndex = 2;
+            // 
+            // prgb_durum
+            // 
+            this.prgb_durum.Location = new System.Drawing.Point(82, 253);
+            this.prgb_durum.Name = "prgb_durum";
+            this.prgb_durum.Size = new System.Drawing.Size(307, 23);
+            this.prgb_durum.TabIndex = 3;
+            // 
+            // btn_analizet
+            // 
+            this.btn_analizet.Location = new System.Drawing.Point(82, 282);
+            this.btn_analizet.Name = "btn_analizet";
+            this.btn_analizet.Size = new System.Drawing.Size(75, 23);
+            this.btn_analizet.TabIndex = 4;
+            this.btn_analizet.Text = "Analiz Et";
+            this.btn_analizet.UseVisualStyleBackColor = true;
+            this.btn_analizet.Click += new System.EventHandler(this.btn_analizet_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(501, 312);
+            this.Controls.Add(this.btn_analizet);
+            this.Controls.Add(this.prgb_durum);
+            this.Controls.Add(this.lstb_analizsonucu);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tx_analizmetin);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tx_analizmetin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lstb_analizsonucu;
+        private System.Windows.Forms.ProgressBar prgb_durum;
+        private System.Windows.Forms.Button btn_analizet;
+    }
+}
+
---
+++ Frekans Analizi/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Frekans_Analizi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_analizet_Click(object sender, EventArgs e)
+        {
+            int[] alfabe = new int[26];
+            //0,0,0,0,0,0,0...
+            //a,b,c,d,e,f,g...
+            prgb_durum.Maximum = tx_analizmetin.TextLength;
+            for (int i = 0; i < tx_analizmetin.TextLength; i++)
+            {
+                if(tx_analizmetin.Text[i] > 96 && tx_analizmetin.Text[i] < 123) // örnek : a = 97 - 97 = 0
+                {
+                    alfabe[tx_analizmetin.Text[i] - 97] += 1; // 
+                }
+                prgb_durum.Value += 1;
+            }
+
+            for(int a = 0; a < alfabe.Length; a++)
+            {
+                char harf = (char)(a + 97);
+                lstb_analizsonucu.Items.Add(harf.ToString() + " = " + alfabe[a].ToString());
+            }
+
+        }
+    }
+}
